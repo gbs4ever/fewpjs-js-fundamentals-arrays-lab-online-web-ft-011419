a98a82c541c22be4e8b898ccc4327c38cc4c4078
Simcha Greenbaum <gbs4ever2@gmail.com>
2019-12-23 23:58:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ const append=  ["Milo", "Otis", "Garfield"]
 const  prepend = ["Milo", "Otis", "Garfield"]
  const removeLast = ["Milo", "Otis", "Garfield"]
  const removeFirst = ["Milo", "Otis", "Garfield"]
-fucntion removeFirst() {
 
-}
